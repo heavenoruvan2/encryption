9517bb71c1504c792b2399dcda174b070a6b6e81
heavenoruvan2 <heavenoruvan@gmail.com>
2026-07-01 19:43:08 +0530

    Create cipher.py

---
+++ cipher.py
@@ -0,0 +1,15 @@
+def encrypt(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - start + shift) % 26 + start)
+        else:
+            result += char
+
+    return result
+
+
+def decrypt(text, shift):
+    return encrypt(text, -shift)
